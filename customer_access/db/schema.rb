0b0c96e00c593bc54a8dad5570bbe3537a6e696b
kangxiaoya <1257333020kxy@gmail.com>
2014-06-06 12:30:00 +0800

    Add admin attributes to the administrator

--- customer_access/db/schema.rb
+++ customer_access/db/schema.rb
@@ -11,18 +11,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140529081939) do
+ActiveRecord::Schema.define(version: 20140606025025) do
 
   create_table "users", force: true do |t|
     t.string   "name"
-    t.string   "password"
-    t.string   "password_confirmation"
     t.string   "password_digest"
     t.string   "forget_password_question"
     t.string   "question_answer"
     t.string   "token"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "admin"
   end
 
 end
